7849f36af86c31d5921474b1bd10dec9d4d3d77b
Mohammed-Boutahar <mohammed.boutahar@mines-ales.org>
2024-03-23 21:50:28 -0400

    chatGPT in cli

---
+++ server/openai-api-cli.py
@@ -0,0 +1,22 @@
+from openai import OpenAI
+
+conversation_history = []
+
+client = OpenAI()
+
+while True:
+    message = str(input("\n"))
+    if(message == "quit" or message == ""):
+        break
+    else:
+        completion = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[{"role": "user", "content": message}],
+                max_tokens=150
+            )
+        response_content = completion.choices[0].message.content
+        conversation_history.append({"role": "assistant", "content": response_content})
+        print(response_content)
+
+
+
