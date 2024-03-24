74c493f408a579896487834dd0768e4c010fe72c
Mohammed-Boutahar <mohammed.boutahar@mines-ales.org>
2024-03-23 21:57:32 -0400

    new branch no logo no context, just conv

--- client/src/app/modules/shared/components/sidebar/sidebar.component.ts
+++ client/src/app/modules/shared/components/sidebar/sidebar.component.ts
@@ -13,17 +13,6 @@ import { NgClass } from '@angular/common';
 export class SidebarComponent {
   faPlus = faPlus;
   conversationList: String[] = [
-    'Conversation 1',
-    'Conversation 2',
-    'Conversation 3',
-    'Conversation 4',
-    'Conversation 5',
-    'Conversation 6',
-    'Conversation 7',
-    'Conversation 8',
-    'Conversation 9',
-    'Conversation 10',
-
   ];
 
   isHovered = false;
